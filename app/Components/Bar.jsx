6a4638d3a60d951267e53cc0dcf60d225f3dea6b
unknown <mayanktilwankar2355@gmail.com>
2024-09-27 23:07:28 +0530

    asd

--- app/Components/Bar.jsx
+++ app/Components/Bar.jsx
@@ -6,7 +6,7 @@ const FixedBar = () => {
         className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-black bg-[#EDA915]">
         <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#075593] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
         <span className="relative  text-black transition duration-300 group-hover:text-white ease">
-          Book Your Demo class now for free"{" "}
+          Book Your Demo class now for free{" "}
         </span>
       </a>
     </div>

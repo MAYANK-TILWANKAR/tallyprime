4499a89b04421c66787120772962f35564dca0ad
mayanktilwankar2355@gmail.com <mayanktilwankar2355@gmail.com>
2024-11-14 19:07:23 +0530

    fiamal

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -58,31 +58,25 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
-        <script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1609493679586075');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+
+        <Script id="fb-pixel">{`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1609493679586075');
+fbq('track', 'PageView');
+`}</Script>
         <noscript>
           <img
             height="1"
             width="1"
-            style={{ display: "none" }}
+            style="display:none"
             src="https://www.facebook.com/tr?id=1609493679586075&ev=PageView&noscript=1"
-            alt=""
           />
         </noscript>
       </head>

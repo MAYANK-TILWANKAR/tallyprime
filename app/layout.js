17286084839184d31143fe7e0a8884bdfa8886c9
mayanktilwankar2355@gmail.com <mayanktilwankar2355@gmail.com>
2024-11-14 13:51:11 +0530

    fiamal

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-import Script from "next/script";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,84 +10,82 @@ export const metadata = {
     "Master TallyPrime with GST in 3 months. 100% job placement support. Comprehensive accounting software course by ICA Global Academy. Enroll now!",
   keywords:
     "TallyPrime, GST, accounting software, ICA Global Academy, job placement, accounting course",
-  author: "ICA Global Academy",
+  authors: [{ name: "ICA Global Academy" }],
   openGraph: {
     title: "TallyPrime Course | Learn Accounting Software | ICA Global Academy",
     description:
       "Master TallyPrime with GST in 3 months. 100% job placement support. Comprehensive accounting software course by ICA Global Academy. Enroll now!",
     type: "website",
     url: "https://www.icaglobalacademy.com/tallyprime-course",
-    image: "/images/tallyprime-course-og.jpg",
+    images: [{ url: "/images/tallyprime-course-og.jpg" }],
   },
   twitter: {
     card: "summary_large_image",
     title: "TallyPrime Course | Learn Accounting Software | ICA Global Academy",
     description:
       "Master TallyPrime with GST in 3 months. 100% job placement support. Comprehensive accounting software course by ICA Global Academy. Enroll now!",
-    image: "/images/tallyprime-course-twitter.jpg",
+    images: ["/images/tallyprime-course-twitter.jpg"],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <header>
-        <Head>
-          <title>
-            TallyPrime Course | Learn Accounting Software | ICA Global Academy
-          </title>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <link
-            rel="canonical"
-            href="https://www.icaglobalacademy.com/tallyprime-course"
-          />
-          <meta name="robots" content="index, follow" />
-          <meta name="googlebot" content="index, follow" />
-          <meta name="google" content="notranslate" />
-          <script type="application/ld+json">
-            {`
-            {
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="canonical"
+          href="https://www.icaglobalacademy.com/tallyprime-course"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="notranslate" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "Course",
-              "name": "TallyPrime Course",
-              "description": "Master TallyPrime with GST in 3 months. 100% job placement support.",
-              "provider": {
+              name: "TallyPrime Course",
+              description:
+                "Master TallyPrime with GST in 3 months. 100% job placement support.",
+              provider: {
                 "@type": "Organization",
-                "name": "ICA Global Academy",
-                "sameAs": "https://www.icaglobalacademy.com"
-              }
-            }
-          `}
-          </script>
-        </Head>
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`
-              
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1609493679586075');
-fbq('track', 'PageView');
-              `}
-        </Script>
+                name: "ICA Global Academy",
+                sameAs: "https://www.icaglobalacademy.com",
+              },
+            }),
+          }}
+        />
+        <script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1609493679586075');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
         <noscript>
           <img
             height="1"
             width="1"
-            style="display:none"
+            style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1609493679586075&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
-      </header>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

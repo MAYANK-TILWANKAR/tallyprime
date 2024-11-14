5950228b6c179f927b713ba85cf0eedae6680712
mayanktilwankar2355@gmail.com <mayanktilwankar2355@gmail.com>
2024-11-14 19:08:52 +0530

    fiamal

--- app/layout.js
+++ app/layout.js
@@ -75,7 +75,7 @@ fbq('track', 'PageView');
           <img
             height="1"
             width="1"
-            style="display:none"
+            style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1609493679586075&ev=PageView&noscript=1"
           />
         </noscript>
